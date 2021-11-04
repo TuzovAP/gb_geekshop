e2ccf53459b20973d213a0adc91dc1c7306eb7ef
Aleksandr <tuzovap@gmail.ru>
2021-11-04 13:18:37 +0300

    HomeWork06

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.http import JsonResponse
 from mainapp.models import Product
 from basketapp.models import Basket
+from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
 
-
+@login_required
 def basket(request):
-    pass
+    basket_list = Basket.objects.filter(user=request.user)
+    context = {
+        'baskets': basket_list
+    }
+    return render(request, 'basketapp/basket.html', context)
 
-
+@login_required
 def add(request, pk):
     product_item = get_object_or_404(Product, pk=pk)
     basket_items = Basket.objects.filter(product=product_item, user=request.user).first()
@@ -16,5 +23,26 @@ def add(request, pk):
     basket_items.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+@login_required
 def remove(request, pk):
-    pass
+    basket_item = get_object_or_404(Basket, pk=pk)
+    basket_item.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def edit(request, pk, quantity):
+    if request.is_ajax():
+        quantity = int(quantity)
+        basket_item = Basket.objects.get(pk=pk)
+        if quantity > 0:
+            basket_item.quantity = quantity
+            basket_item.save()
+        else:
+            basket_item.delete()
+        basket_list = Basket.objects.filter(user=request.user)
+        context = {
+            'baskets': basket_list
+        }
+        result = render_to_string('basketapp/includes/inc_basket_list.html', context)
+        return JsonResponse({'result': result})
